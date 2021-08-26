ec134f24f5096ff14ef56c63ba10e0a9f99cae2b
friendlystone <georgiana.bumbar@yahoo.com>
2021-08-26 14:22:48 +0300

    Last modif

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,8 +5,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   View,
-  TextInput,
   Text,
+  FlatList 
 } from 'react-native';
 
 const Drawer = createDrawerNavigator();
@@ -19,16 +19,8 @@ const date = new Date();
 
 export default class HomeScreen extends React.Component {
 
-    state = {
-        search: '',
-      };
-    
-    updateSearch = (search) => {
-        this.setState({ search });
-    };
-
     render() {
-        const { search } = this.state;
+        
         return (
             <View> 
                 <View style = {{ backgroundColor: '#526aa8', paddingBottom: 10, paddingTop: 50 }}> 
@@ -48,9 +40,9 @@ export default class HomeScreen extends React.Component {
                     </View>
 
                     <View style = {{ flex: 1, paddingBottom: 35, margin: 5 }}>
-                        <View style = {{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', width: 320, height: 32, alignSelf: 'center', borderRadius: 30, paddingBottom: 10 }}>
+                        <View style = {{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', width: 450, height: 35, alignSelf: 'center', borderRadius: 30, paddingBottom: 10 }}>
                            
-                            <Text style = {{ color: '#526aa8',  fontWeight: 'bold', textAlign: 'center', marginLeft: 130, marginTop: 2, fontSize: 18 }} >
+                            <Text style = {{ color: '#526aa8',  fontWeight: 'bold', textAlign: 'center', marginLeft: 190, marginTop: 4, fontSize: 20 }} >
                                 Search
                             </Text>
                             <TouchableOpacity>
@@ -64,43 +56,42 @@ export default class HomeScreen extends React.Component {
                     </View>
 
                     <View>
-                        <Text style = {{ color: 'white', fontWeight: 'bold', marginLeft: 10, marginBottom: 15, fontSize: 20}}>
+                        <Text style = {{ color: 'white', fontWeight: 'bold', marginLeft: 30, marginBottom: 15, fontSize: 20}}>
                             { monthNames[date.getMonth()] + ' ' + date.getFullYear()  }
                         </Text>
                     
 
                     <View style = { styles.calendarComponents }>
-                                
                                 <Text 
                                     style = { styles.dateContainers }
                                 >
-                                    { 26 + '\n ' + dayNames[(date.getDay())%10-1] }
+                                    { 26 + '\n' + dayNames[(date.getDay())%10-1] }
                                 </Text>
 
                                 <Text 
                                     style = { styles.dateContainers }
                                 >
-                                    { 27 + '\n ' + dayNames[(date.getDay())%10] }
+                                    { 27 + '\n' + dayNames[(date.getDay())%10] }
                                 </Text>
 
                                 <Text 
                                     style = { styles.dateContainers }
                                 >
-                                    { 28 + '\n ' + dayNames[(date.getDay())%10+1] }
+                                    { 28 + '\n' + dayNames[(date.getDay())%10+1] }
                                 </Text>
 
                                 <Text 
                                     style = { styles.dateContainers }
                                 >
-                                    { 29 + '\n ' + dayNames[(date.getDay())%10+2] }
+                                    { 29 + '\n' + dayNames[(date.getDay())%10+2] }
                                 </Text> 
                     </View>
                     </View>
                 </View>
-                <View style = {{ backgroundColor: 'lightgrey', width: 350, height: 70, alignSelf: 'center', borderRadius: 10, paddingBottom: 15, marginTop: 20, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style = {{ backgroundColor: '#DCDCDC', width: 470, height: 80, alignSelf: 'center', borderRadius: 10, paddingBottom: 15, marginTop: 20, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Text style = {{ alignSelf: 'flex-start', color: 'grey', paddingTop: 5, paddingRight: 10, paddingLeft: 10, fontWeight: 'bold', fontSize: 18, paddingLeft: 19, flexDirection: 'column' }}>
                         New Task
-                        <Text style = {{ fontWeight: 'normal', fontSize: 12, color: '#787878' }}>
+                        <Text style = {{ fontWeight: 'normal', fontSize: 12, color: '#C0C0C0' }}>
                             {'\n'}Lorem ipsum dolor sit amet, consectetur {'\n'}adipiscing elit. Praesent a tristique ipsum.  
                         </Text>
                     </Text>
@@ -110,59 +101,36 @@ export default class HomeScreen extends React.Component {
                         />
                     </TouchableOpacity>
                 </View>
-                <View style = {{ paddingTop: 25 }}>
-                        <View style = {{ flexDirection: 'row', justifyContent: 'space-evenly', padding: 5, alignItems: 'center'}}>
-                            <View style = {{ marginBottom: 5, }} >
-                                <Image style = {{ width: 40, height: 40, alignSelf: 'flex-end', paddingTop: 20 }}
-                                    source = { require('../assets/users.png') }
-                                />
-                                <Text style = {{ color: 'lightgrey', textAlign: 'center' }}>
-                                    Name
-                                </Text>
-                            </View>
-                             <View style = {{ backgroundColor: 'lightgrey', width: 250, height: 70, alignSelf: 'center', borderRadius: 10, paddingBottom: 10 }}>
-
-                             </View>
+                <View style = { styles.containers }>
+                    <FlatList
+                        data = {[
+                        {key: 'Devin'},
+                        {key: 'Dan'},
+                        {key: 'Dominic'},
+                        {key: 'Jackson'},
+                        ]}
+                        style = {{ height: 400 }}
+                        renderItem={({item}) => <Text style = { styles.item }> 
+                        {'\u2022'}  <View style = {{ flexDirection: 'row', justifyContent: 'space-evenly', padding: 5, alignItems: 'center'}}>
+                        <View style = {{ marginBottom: 5}} >
+                            <Image style = {{ width: 45, height: 45, alignSelf: 'flex-end', paddingTop: 20, marginRight: 20, marginTop: 10 }}
+                                source = { require('../assets/users.png') }
+                            />
+                            <Text style = {{ color: 'lightgrey', textAlign: 'center', marginRight: 20, fontSize: 15 }}>
+                                Name
+                            </Text>
                         </View>
-                        <View style = {{ flexDirection:'row', justifyContent: 'space-evenly', padding: 5, alignItems: 'center'}}>
-                            <View style = {{ marginBottom: 5 }}>
-                                <Image style = {{ width: 40, height: 40, alignSelf: 'flex-end', paddingTop: 20 }}
-                                    source = { require('../assets/users.png') }
-                                />
-                                <Text style = {{ color: 'lightgrey', textAlign: 'center' }}>
-                                    Name
+                         <View style = {{ backgroundColor: '#DCDCDC', width: 360, height: 70, alignSelf: 'center', borderRadius: 10, paddingBottom: 10, justifyContent: 'flex-start' }}>
+                                <Text style = {{ fontSize: 15, color: 'grey', marginLeft: 10, marginTop: 2, textAlign: 'left', fontWeight: 'bold'  }}>
+                                    Lorem Ipsum
                                 </Text>
-                            </View>
-                             <View style = {{ backgroundColor: 'lightgrey', width: 250, height: 70, alignSelf: 'center', borderRadius: 10, paddingBottom: 10 }}>
-
-                             </View>
-                        </View>
-                        <View style = {{ flexDirection:'row', justifyContent: 'space-evenly', padding: 5, alignItems: 'center'}}>
-                             <View style = {{ marginBottom: 5 }}>
-                                <Image style = {{ width: 40, height: 40, alignSelf: 'flex-end', margin: -2, marginTop: 5, paddingTop: 5 }}
-                                    source = { require('../assets/users.png') }
-                                />
-                                <Text style = {{ color: 'lightgrey', textAlign: 'center' }}>
-                                    Name
+                                <Text style = {{ fontSize: 13, color: '#C0C0C0', marginLeft: 10, textAlign: 'left' }}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                                 </Text>
-                            </View>
-                             <View style = {{ backgroundColor: 'lightgrey', width: 250, height: 70, alignSelf: 'center', borderRadius: 10, paddingBottom: 10 }}>
-
-                             </View>
-                        </View>
-                        <View style = {{ flexDirection:'row', justifyContent: 'space-evenly', padding: 5, alignItems: 'center'}}>
-                            <View style = {{ marginBottom: 5 }}>
-                                <Image style = {{ width: 40, height: 40, alignSelf: 'flex-end', paddingTop: 20 }}
-                                    source = { require('../assets/users.png') }
-                                />
-                                <Text style = {{ color: 'lightgrey', textAlign: 'center' }}>
-                                    Name
-                                </Text>
-                            </View>
-                             <View style = {{ backgroundColor: 'lightgrey', width: 250, height: 70, alignSelf: 'center', borderRadius: 10, paddingBottom: 10 }}>
-
-                             </View>
-                        </View>
+                         </View>
+                    </View>                        
+                    </Text>}
+                    />
                 </View>
             </View>
         );
@@ -176,6 +144,21 @@ const styles = StyleSheet.create({
         height: '100%',
         flexDirection: 'row',
         justifyContent: 'space-evenly',
+    },
+    item: {
+        padding: 6,
+        fontSize: 40,
+        height: 80,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        color: '#3bbca6'
+    },
+    containers: {
+        flex: 1,
+        paddingTop: 10,
+        height: 100,
+        textAlign: 'center',
+        flexDirection: 'column'
     },
     calendarComponents: {
         flexDirection: 'row',
@@ -235,18 +218,16 @@ const styles = StyleSheet.create({
         borderLeftWidth: 1,
         borderBottomWidth: 1,
         borderRightWidth: 1,
-        width: 70,
-        height: 70,
+        width: 100,
+        height: 100,
         resizeMode: 'stretch',
-        alignSelf: 'center',
-        alignItems: 'center',
         textAlign: 'center',
         color: '#526aa8',
         borderRadius: 10,
         fontWeight: 'bold',
         backgroundColor: 'white',
-        fontSize: 22,
-        paddingRight: 4,
+        fontSize: 28,
+        paddingRight: 8,
         paddingTop: 5
     }
 })
